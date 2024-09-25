f81ecbced08ef0d08ea4ca04b8922cdde4e60e42
f1fti0 <pdima2005@mail.ru>
2024-09-25 14:55:10 +0300

    lab-02

--- lab_02/lab_02_oop.py
+++ lab_02/lab_02_oop.py
@@ -1,23 +1,29 @@
 import math
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Figure(ABC):
-    def __init__(self, COLOR):
-        self.color = COLOR
-
+    @abstractmethod
     def square(self):
         pass
 
 
-class Circle(Figure):
-    def __init__(self, RADIUS, COLOR):
-        super().__init__(COLOR)
-        self.name = "Circle"
-        self.radius = RADIUS
+class ColorFigure:
+    def __init__(self, COLOR):
+        self.color = COLOR
 
     def __repr__(self):
-        return f'Figure: {self.name}, Radius: {self.radius}, Color: {self.color}'
+        return f'{self.color}'
+
+
+class Circle(Figure):
+    def __init__(self, RADIUS, COLOR):
+        self.name = "Circle"
+        self.radius = RADIUS
+        self.color = ColorFigure(COLOR)
+
+    def __repr__(self):
+        return f'Figure: {self.name}, Radius: {self.radius}, Color: {self.color}, Square: {self.square()}'
 
     def square(self):
         return math.pi * (self.radius ** 2)
@@ -25,13 +31,13 @@ class Circle(Figure):
 
 class Rectangle(Figure):
     def __init__(self, HEIGHT, WIDTH, COLOR):
-        super().__init__(COLOR)
         self.name = "Rectangle"
         self.height = HEIGHT
         self.width = WIDTH
+        self.color = ColorFigure(COLOR)
 
     def __repr__(self):
-        return f'Figure: {self.name}, Height: {self.height}, Width: {self.width}, Color: {self.color}'
+        return f'Figure: {self.name}, Height: {self.height}, Width: {self.width}, Color: {self.color}, Square: {self.square()}'
 
     def square(self):
         return self.width * self.height
@@ -41,6 +47,7 @@ class Square(Rectangle):
     def __init__(self, HEIGHT, COLOR):
         super().__init__(HEIGHT, HEIGHT, COLOR)
         self.name = "Square"
+        self.color = ColorFigure(COLOR)
 
     def __repr__(self):
-        return f'Figure: {self.name}, Side of square: {self.height}, Color: {self.color}'
+        return f'Figure: {self.name}, Side of square: {self.height}, Color: {self.color}, Square: {self.square()}'
